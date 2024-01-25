86f72002d9e489b204d92a016b073cf8ee15e961
Brian Foster <brianisbrilliant@users.noreply.github.com>
2024-01-25 10:51:15 -0600

    Added Keva tower and button to control it with.

---
+++ Assets/ButtonAnim.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAnim : MonoBehaviour
+{
+    public string triggerName = "NextAnim";
+    Animator anim;
+
+    void Start() {
+        anim = this.GetComponent<Animator>();
+    }
+
+    public void NextAnim() {
+        anim.SetTrigger(triggerName);
+    }
+}
